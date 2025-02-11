b8cbb39fa72123c6ce014ca5fd1c3aac88c6c333
Usama Al-Zayan <usama.alzayan@tentimejo.com>
2025-02-11 19:27:24 +0200

    Building a reusable components

---
+++ 07-usepopcorn/src/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRatig";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <Test />
+  </React.StrictMode>
+);
+
+function Test() {
+  const [movieRating, setMoveRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" onSetRating={setMoveRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
