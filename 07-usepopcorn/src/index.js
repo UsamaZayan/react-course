923c85de01fc6ca2af76fbcdfe245a91b89a72d2
Usama Al-Zayan <usama.alzayan@tentimejo.com>
2025-02-25 22:55:43 +0200

    Effects and Data Fetching

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,28 +1,29 @@
-import React, { useState } from "react";
+//import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRatig";
+import "./index.css";
+import App from "./App";
+//import StarRating from "./StarRatig";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
       defaultRating={3}
-    />
-    <Test />
+    /> */}
+    {/* <Test /> */}
   </React.StrictMode>
 );
 
-function Test() {
-  const [movieRating, setMoveRating] = useState(0);
-  return (
-    <div>
-      <StarRating color="blue" onSetRating={setMoveRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+// function Test() {
+//   const [movieRating, setMoveRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating color="blue" onSetRating={setMoveRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
